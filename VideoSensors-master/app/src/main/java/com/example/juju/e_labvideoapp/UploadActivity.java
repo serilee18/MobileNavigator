b725814d9faffc3c501ad41f826ad758ebfdcda1
serilee <tpfl27@korea.ac.kr>
2019-02-23 18:20:05 +0900

    update

--- VideoSensors-master/app/src/main/java/com/example/juju/e_labvideoapp/UploadActivity.java
+++ VideoSensors-master/app/src/main/java/com/example/juju/e_labvideoapp/UploadActivity.java
@@ -15,40 +15,50 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class UploadActivity extends AppCompatActivity {
 
+
+    Handler handler = new Handler();
+
+    ArrayList<Bitmap> BitmapArr = new ArrayList<Bitmap>();
+
+    ArrayList<ImageView> ImageArr = new ArrayList<ImageView>();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-
-
-        final Handler handler = new Handler();
 
         Intent intent = getIntent();
         final String message = intent.getStringExtra("FilePath");
 
         Log.d("CSV PATH", message);
         final ProgressBar uploadPB = (ProgressBar) findViewById(R.id.uploadPB);
-//        final ImageView ResultImage1 = (ImageView) findViewById(R.id.ResultImage1) ;
-//        final ImageView ResultImage2 = (ImageView) findViewById(R.id.ResultImage2) ;
-//        final ImageView ResultImage3 = (ImageView) findViewById(R.id.ResultImage3) ;
-//        final ImageView ResultImage4 = (ImageView) findViewById(R.id.ResultImage4) ;
-//        final ImageView ResultImage5 = (ImageView) findViewById(R.id.ResultImage5) ;
-//        final ImageView ResultImage6 = (ImageView) findViewById(R.id.ResultImage6) ;
-//        final ImageView ResultImage7 = (ImageView) findViewById(R.id.ResultImage7) ;
-//        final ImageView ResultImage8 = (ImageView) findViewById(R.id.ResultImage8) ;
-//        final ImageView ResultImage9 = (ImageView) findViewById(R.id.ResultImage9) ;
-//        final ImageView ResultImage10 = (ImageView) findViewById(R.id.ResultImage10) ;
-//        final ImageView ResultImage11 = (ImageView) findViewById(R.id.ResultImage11) ;
-//        final ImageView ResultImage12 = (ImageView) findViewById(R.id.ResultImage12) ;
+        final Button RestartButton = (Button) findViewById(R.id.RestartButton);
 
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage1)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage2)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage3)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage4)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage5)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage6)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage7)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage8)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage9)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage10)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage11)) ;
+        ImageArr.add((ImageView) findViewById(R.id.ResultImage12)) ;
+
+
+        findViewById(R.id.RestartButton).setOnClickListener(mClickListener);
 
 
         uploadPB.setVisibility(View.VISIBLE);
-
+        RestartButton.setVisibility(View.GONE);
         Thread mThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -67,20 +77,23 @@ public class UploadActivity extends AppCompatActivity {
                     String[] ResultImage = response.get(0).split(",");
 
                     for(int i=1;i<ResultImage.length;i++) {
-                        Log.d("ResultImage", ResultImage[i]+" + "+i );
-                        int resID = getResources().getIdentifier("ResultImage"+i, "id", getPackageName());
-                        final ImageView iv = (ImageView) findViewById(resID);
                         URL url = new URL("http://163.152.217.166:8888/static/result/"+ResultImage[i]+".jpg");
                         InputStream is = url.openStream();
-                        final Bitmap bm = BitmapFactory.decodeStream(is);
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {  // 화면에 그려줄 작업
-                                iv.setImageBitmap(bm);
-                            }
-                        });
-                        iv.setImageBitmap(bm); //비트맵 객체로 보여주기
+                        BitmapArr.add(BitmapFactory.decodeStream(is));
+                        //final Bitmap bm[i] = BitmapFactory.decodeStream(is);
                     }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {  // 화면에 그려줄 작업
+                            uploadPB.setVisibility(View.GONE);
+                            RestartButton.setVisibility(View.VISIBLE);
+                            for(int i=0;i<BitmapArr.size();i++) {
+                                Bitmap bitmap_tmp = BitmapArr.get(i);
+                                ImageArr.get(i).setImageBitmap(bitmap_tmp);
+                            }
+                        }
+                    });
+
 
                 } catch (Exception ex) {
                     ex.printStackTrace();
@@ -92,4 +105,13 @@ public class UploadActivity extends AppCompatActivity {
         mThread.start();
 
     }
+
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
+
 }
